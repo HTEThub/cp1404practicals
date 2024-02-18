e1a619a60658a2f5a8fc9f9ab65ef94abe615439
htetg <htetmyet@my.jcu.edu.au>
2024-02-18 11:20:34 +1000

    Got to the last step of the exercise. Added prices_output.txt for capitalist_conrad.py outputs.

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,15 +9,18 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+day = 0
+OUTPUT_FILE = "prices_output.txt"
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+print(f"Starting price: ${price:,.2f}")
 
+out_file = open(OUTPUT_FILE, 'w')
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
     # generate a random integer of 1 or 2
@@ -30,6 +33,7 @@ while MIN_PRICE <= price <= MAX_PRICE:
         # generate a random floating-point number
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
-
+    day += 1
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {day} price is ${price:,.2f}", file=out_file)
+out_file.close()
