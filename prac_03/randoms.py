bcb0bd069ae41e7c37a6afe8983cb9ef34553d25
htetg <htetmyet@my.jcu.edu.au>
2024-02-18 11:00:22 +1000

    Finished randoms.py exercise with answers to question produced random number between 1 and 100 inclusive

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+""" Questions """
+# 1. In line 1, I saw the number 16.
+#    The smallest number I could have seen is 5 and the largest is 20.
+
+# 2. In line 2, I saw the number 9.
+#    The smallest number I could have seen is 3 and the largest is 9.
+#    No, the line 2 could not produce 4. Only odd numbers or n+2 (starting with odd)
+
+# 3. In line 3, I saw the number 3.86648056652077.
+#    The smallest number I could have seen is 2.5 and the largest is 5.5.
+
+import random
+print(random.randint(1, 100))
