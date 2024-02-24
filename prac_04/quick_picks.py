adfd2b0c20f9d0c7dae5f98151c73515e00f4334
htetg <htetmyet@my.jcu.edu.au>
2024-02-24 21:41:15 +1000

    Finished '"Quick Pick" Lottery Ticket Generator' final exercise

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,17 @@
+import random
+
+MIN_NUM = 1
+MAX_NUM = 45
+MAX_RANDOM_NUM_COUNT = 6
+
+
+num_of_picks = int(input("How many quick picks?: "))
+for eachPick in range(num_of_picks):
+    random_numbers = []
+    for eachNumber in range(MAX_RANDOM_NUM_COUNT):
+        random_number = random.randint(MIN_NUM, MAX_NUM)
+        while random_number in random_numbers:
+            random_number = random.randint(MIN_NUM, MAX_NUM)
+        random_numbers.append(random_number)
+        print(f"{random_number:2}", end=" ")
+    print()
