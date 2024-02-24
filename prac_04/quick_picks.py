395d71a42915cbf4dd31bc13a8c9593d49a5ee33
htetg <htetmyet@my.jcu.edu.au>
2024-02-24 21:58:21 +1000

    Added a requirement that I missed.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,10 +8,15 @@ MAX_RANDOM_NUM_COUNT = 6
 num_of_picks = int(input("How many quick picks?: "))
 for eachPick in range(num_of_picks):
     random_numbers = []
+
     for eachNumber in range(MAX_RANDOM_NUM_COUNT):
         random_number = random.randint(MIN_NUM, MAX_NUM)
         while random_number in random_numbers:
             random_number = random.randint(MIN_NUM, MAX_NUM)
         random_numbers.append(random_number)
-        print(f"{random_number:2}", end=" ")
+
+    random_numbers.sort()
+
+    for eachElement in random_numbers:
+        print(f"{eachElement:2}", end=" ")
     print()
