18224ff268c7f939b52453861eec05e9622fdf73
htetg <htetmyet@my.jcu.edu.au>
2024-03-27 02:15:07 +1000

    Updated it so that the date filter function can work

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,14 +1,20 @@
+import datetime
+
+
 class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion):
         self.name = name
-        self.start_date = start_date
+        if isinstance(start_date, str):
+            self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
+        else:
+            self.start_date = start_date
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion = completion
 
     # Implement methods for sorting/comparing Project objects based on priority order
     def __lt__(self, other):
-        return self.priority < other.priority
+        return self.start_date < other.start_date
 
     def __str__(self):
         return f"{self.name}, start: {self.start_date}, priority {self.priority}, " \
