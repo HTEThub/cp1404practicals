e7e68317ada24146f339ec5dd10e2347345fcc81
htetg <htetmyet@my.jcu.edu.au>
2024-03-26 22:08:37 +1000

    Finished half of intermediate exercise More Guitars!

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -5,21 +5,19 @@ FILENAME = "guitars.csv"
 
 def main():
     guitars = []
-    read_guitars_csv(FILENAME)
+    read_guitars_csv(FILENAME, guitars)
+    guitars.sort()
+    for eachGuitar in guitars:
+        print(eachGuitar)
 
 
-def read_guitars_csv(filename):
-    guitars = []
+def read_guitars_csv(filename, guitars):
     in_file = open(filename, 'r', newline='')
     for eachLine in in_file:
         split_line = eachLine.strip().split(',')
-        guitars.append(str(Guitar(split_line[0], split_line[1], split_line[2])))
+        guitar = Guitar(split_line[0], split_line[1], split_line[2])
+        guitars.append(guitar)
     in_file.close()
-
-    print(guitars)
-
-
-
 
 
 main()
