8679953c5639bd28b320af682e45023c06092c6d
htetg <htetmyet@my.jcu.edu.au>
2024-04-07 22:10:51 +1000

    Fixed formatting

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -6,7 +6,8 @@ from kivy.uix.label import Label
 class DynamicLabelsApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.name_to_phone = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456"}
+        self.name_to_phone = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456",
+                              "LOL": "101"}
 
     def build(self):
         self.title = "Dynamic Labels"
